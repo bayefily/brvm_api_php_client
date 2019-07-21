5539425a4e57e33cdd73f6d0c0d2e08c780a5c46
= <xaralaxarala@gmail.com>
2019-07-21 22:00:29 +0000

    Add details page

--- index.php
+++ index.php
@@ -20,49 +20,21 @@ function get_data(){
     $gco = $client->GET_COURS_OBLIGATIONS();
     $data_gco = $gco->ClassCoursObligations;
     $gir = $client->GET_INDICES_REFERENCE();
-    $data_gir = $gir->ClassCoursObligations;
-
-
-  $data_slice_gca = array_slice($data_gca, 0, 5);
-  $data_slice_gco = array_slice($data_gco, 0, 5);
-  $data_slice_gir = array_slice($data_gir, 0, 5);
-
-  echo'<div class="col-md-4">';
-  echo '<table class="table">';
-  echo '<thead class="thead-dark"><tr><th>top 5</td><th>Cloture</th><th>Variation</th></tr></thead>';
-  foreach ($data_slice_gca as $item) {
-      echo '<tr>';
-      echo '<td>'.$item->Code_symbole.'</td>';
-      echo '<td>'.number_format((float)$item->Cours_Cloture, 2, '.', '').'</td>';
-      echo '<td>'.number_format((float)$item->Variation_Pourcentage, 2, '.', '') ."%".'</td>';
-      echo '</tr>';
-  }
-  echo '</table>';
-  echo'</div>';
-  echo'<div class="col-md-4">';
-  echo '<table class="table">';
-  echo '<thead class="thead-dark"><tr><th>top 5</td><th>Cloture</th><th>Variation</th></tr></thead>';
-  foreach ($data_slice_gca as $item) {
-      echo '<tr>';
-      echo '<td>'.$item->Code_symbole.'</td>';
-      echo '<td>'.number_format((float)$item->Cours_Cloture, 2, '.', '').'</td>';
-      echo '<td>'.number_format((float)$item->Variation_Pourcentage, 2, '.', '') ."%".'</td>';
-      echo '</tr>';
-  }
-  echo '</table>';
-  echo'</div>';
-  echo'<div class="col-md-4">';
-  echo '<table class="table">';
-  echo '<thead class="thead-dark"><tr><th>top 5</td><th>Cloture</th><th>Variation</th></tr></thead>';
-  foreach ($data_slice_gca as $item) {
-      echo '<tr>';
-      echo '<td>'.$item->Code_symbole.'</td>';
-      echo '<td>'.number_format((float)$item->Cours_Cloture, 2, '.', '').'</td>';
-      echo '<td>'.number_format((float)$item->Variation_Pourcentage, 2, '.', '') ."%".'</td>';
-      echo '</tr>';
-  }
-  echo '</table>';
-  echo'</div>';
+    $data_gir = $gir->ClassIndicesReference;
+    foreach ($data_gca as $item) {
+    echo'<div class="owl-item" style="width: 136px;">';
+        echo'<div class="item">';
+          echo'<span>'.$item->Code_symbole.'</span> ';
+          echo'<span>'.number_format((float)$item->Cours_Cloture, 2, '.', '').'</span> ';
+          echo'<span>'.number_format((float)$item->Variation_Pourcentage, 2, '.', '') .'%'.'</span> ';
+          if ($item->Variation_Pourcentage <= 0) {
+            echo'<span class="icone-seance bad"></span>';
+          } else {
+            echo'<span class="icone-seance good"></span>';
+          }
+        echo'</div>';
+      echo'</div>';
+    }
 
 
   } catch (SoapFault $exception) {
